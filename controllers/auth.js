2062040b73656fd4524349167ec0312e9f629623
Fernando Buono <fbuono@fi.uba.ar>
2022-04-02 20:45:40 -0300

    conexion a la bd

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,25 @@
 const { response } = require('express');
 const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
+const createUser = async(req, res = response) => {
+    // const {name, email, password} = req.body;
 
-const createUser = (req, res = response) => {
-    const {name, email, password} = req.body;
-
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    });
+    try {
+        const user = new User(req.body);
+        await user.save();
+    
+        res.status(201).json({
+            ok: true,
+            msg: 'register',
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el admin',
+        });
+    };
 };
 
 const loginUser = (req, res = response) => {
